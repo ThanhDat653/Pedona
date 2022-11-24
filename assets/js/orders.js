59f8830dbbb9cb24d3b1a88fd9e990edd3007625
Nolb226 <hn26677@gmail.com>
2022-11-24 14:04:39 +0700

    Test : More space to click if Admin want to see orderDetails

--- assets/js/orders.js
+++ assets/js/orders.js
@@ -38,7 +38,7 @@ function baseRenderOrder(i) {
     (i + 1) +
     `
     </div>
-    <div class="col l-8 information">
+    <div class="col l-4 information">
       <div class="row">User Name:  ` +
     orderList[i].name +
     `</div>
@@ -46,35 +46,42 @@ function baseRenderOrder(i) {
     orderList[i].orderID +
     `</div>
     </div>
-	<div class="col l-2 ">
+    <div class="col l-7 click-view ">
+    <div class="row no-gutters end-row">
+    <div class="col l-4 l-o-2 ">
     <span class="check ` +
     (orderList[i].fullyCheck || false) +
     `"> Fully-checked </span>
     </div>
     <div class="col l-1 order-detail__btn">
-      <button class="order-full ` +
+    <button class="order-full ` +
     orderList[i].fullyCheck +
     ` btn"><i class="fa-solid fa-caret-left fa-xl"></i></button>
     </div>
     </div>
-  <div class="details-view no-gutters">
+    </div>
+    </div>
+    <div class="details-view no-gutters">
     
-  </div>`
+    </div>`
   );
 }
 const orderList = gItem("orderList") || defaultOrders;
 function renderOrderDetails() {
+  const orderInformation = document.querySelectorAll(".click-view");
   const orderView = document.querySelectorAll(".details-view");
   const viewButton = document.querySelectorAll(".order-full");
   const OrderNumber = document.querySelectorAll(".order.number");
-  viewButton.forEach((item, indexButton) => {
-    if (item.classList.contains("true")) {
-      item.innerHTML = "✓";
+  orderInformation.forEach((item, indexButton) => {
+    if (viewButton[indexButton].classList.contains("true")) {
+      viewButton[indexButton].innerHTML = "✓";
     } else {
-      item.innerHTML = `<i class="fa-solid fa-caret-left fa-xl"></i>`;
+      viewButton[
+        indexButton
+      ].innerHTML = `<i class="fa-solid fa-caret-left fa-xl"></i>`;
     }
     item.onclick = () => {
-      if (!item.classList.contains("active")) {
+      if (!viewButton[indexButton].classList.contains("active")) {
         let j = 0;
         while (j < viewButton.length) {
           viewButton[j].classList.remove("active");
@@ -83,7 +90,7 @@ function renderOrderDetails() {
           orderView[j].style.height = 0 + "px";
           j++;
         }
-        item.classList.add("active");
+        viewButton[indexButton].classList.add("active");
         orderView[indexButton].classList.add("active");
         orderView[indexButton].innerHTML = "";
         orderList[
@@ -149,7 +156,7 @@ function renderOrderDetails() {
         orderView[indexButton].classList.remove("active");
         orderView[indexButton].style.height = 0 + "px";
         orderView[indexButton].innerHTML = "";
-        item.classList.toggle("active");
+        viewButton[indexButton].classList.toggle("active");
       }
       const checkView = createArr(document.querySelectorAll(".col.check"));
 
@@ -168,7 +175,11 @@ function renderOrderDetails() {
             orderList[indexButton].productList[indexCheck].check = true;
             sItem(orderKey, orderList);
           }
-          fullCheck(orderList[indexButton], indexButton, item);
+          fullCheck(
+            orderList[indexButton],
+            indexButton,
+            viewButton[indexButton]
+          );
         };
       });
     };
